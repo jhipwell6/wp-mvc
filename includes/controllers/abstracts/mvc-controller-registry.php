2948b9f8b34edf43bd70dd08cde07633db526ece
Jason <13647112+jhipwell6@users.noreply.github.com>
2022-11-03 15:23:07 -0400

    Added controller registry

---
+++ includes/controllers/abstracts/mvc-controller-registry.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WP_MVC\Controllers;
+
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+abstract class MVC_Controller_Registry
+{
+	protected static $instances = [];
+
+	public static function instance()
+	{
+		$class = get_called_class();
+		if ( ! isset( self::$instances[$class] ) || ! self::$instances[$class] instanceof $class ) {
+			self::$instances[$class] = new static();
+		}
+		return static::$instances[$class];
+	}
+
+}
